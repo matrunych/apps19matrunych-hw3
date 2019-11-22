4692e1daa225c2cce70c6bd199458d51c7926827
matrunych <47101257+matrunych@users.noreply.github.com>
2019-11-22 16:46:49 +0200

    Update SmartArrayApp.java

--- src/main/java/ua/edu/ucu/SmartArrayApp.java
+++ src/main/java/ua/edu/ucu/SmartArrayApp.java
@@ -95,4 +95,14 @@ public class SmartArrayApp {
         //return Arrays.copyOf(result, result.length, String[].class);
 
     }
+    
+    public static void main(String[] args) {
+        BaseArray a = new BaseArray(new Integer[]{1,2,3,4,5});
+        MapDecorator b = new MapDecorator(a,x->(Integer)x*(Integer)x);
+        System.out.println(Arrays.toString(a.toArray()));
+        System.out.println(Arrays.toString(b.toArray()));
+        a.setValue(0); // що це працювало додайте метод setValue(int index){array[index] = 0};
+        System.out.println(Arrays.toString(a.toArray()));
+        System.out.println(Arrays.toString(b.toArray()));
+    }
 }
